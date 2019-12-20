e348ba0c838777c50b28d89a4fcba60d1849e333
BryanKAdams <17adamsbryk@washk12.org>
2019-12-20 10:10:40 -0700

    Added a useEffect with array dependancy that does a get request to receive colors data from the server and setColorList to the response

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,7 +8,13 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(() => {
+    axiosWithAuth().get("http://localhost:5000/api/colors")
+      .then(res => {
+        console.log(res.data);
+        setColorList(res.data);
+      })
+  }, [setColorList])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
