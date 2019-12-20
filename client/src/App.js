000752b1af7576a8c3ef4599ccb434651a93bdae
BryanKAdams <17adamsbryk@washk12.org>
2019-12-20 10:09:28 -0700

    added unprivateroute and privateroute in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import UnPrivateRoute from "./routes/UnPrivateRoute"
+import PrivateRoute from "./routes/PrivateRoute"
+import BubblePage from "./components/BubblePage"
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+      <UnPrivateRoute exact path="/" component={Login} />
+      <PrivateRoute exact path="/bubbletime" component={BubblePage} />
       </div>
     </Router>
   );
